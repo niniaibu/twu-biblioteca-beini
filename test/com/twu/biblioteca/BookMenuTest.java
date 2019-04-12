72cdddd8f8d4ad54b89f372b7ce946bc5ff4a2a4
Xu Beini <1017578983@qq.com>
2019-04-12 11:18:38 +0800

    rename testBookMenu to BookMenuTest

---
+++ test/com/twu/biblioteca/BookMenuTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BookMenuTest {
+    private BookMenu bookMenu;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUp() {
+        this.bookMenu = new BookMenu();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testBookMenulenght() {
+        assertEquals(bookMenu.getBooks().size(), 4);
+    }
+
+    @Test
+    public void testBookMenuDisplay() {
+        String outputString = "Head First Java\n" +
+                "Effective Java\n" +
+                "Thinking in Java\n" +
+                "Head First Design Patterns\n";
+        bookMenu.displayBooks();
+        assertEquals(outContent.toString(), outputString);
+    }
+
+
+}
--- test/com/twu/biblioteca/testBookMenu.java
+++
@@ -1,4 +0,0 @@
-package com.twu.biblioteca;
-
-public class testBookMenu {
-}
